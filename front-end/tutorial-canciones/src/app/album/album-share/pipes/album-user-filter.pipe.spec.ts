5ca18c264653d4a00b39c19c1f893189ee392395
javargas1098 <ja.vargas1098@gmail.com>
2021-09-08 18:05:20 -0500

    arreglo

--- front-end/tutorial-canciones/src/app/album/album-share/pipes/album-user-filter.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { AlbumUserFilterPipe } from './album-user-filter.pipe';
-
-describe('AlbumUserFilterPipe', () => {
-  it('create an instance', () => {
-    const pipe = new AlbumUserFilterPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
--- front-end/tutorial-canciones/src/app/cancion/cancion-share/pipes/cancion-user-filter.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { CancionUserFilterPipe } from './cancion-user-filter.pipe';
-
-describe('CancionUserFilterPipe', () => {
-  it('create an instance', () => {
-    const pipe = new CancionUserFilterPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
